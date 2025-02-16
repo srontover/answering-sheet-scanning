bc1aa2d95343106a04baaf7e4904661958b85898
sorntover <Xuxue6800@outlook.com>
2025-02-16 13:40:03 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,69 @@
+import cv2 as cv
+import numpy as np
+import scanFunctions as sf
+
+################################
+path = None
+Webcam = True
+height = 640
+width = 480
+cap = cv.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+cap.set(10, 150)
+#################################
+
+sf.initTrackbars()
+count = 0
+
+while True:
+    
+    img_blank = np.zeros((width, height, 3), np.uint8)
+    
+    if Webcam:
+        success, img = cap.read()
+    else:
+        img = cv.imread(path)
+    img = cv.resize(img, (width, height))
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
+    img_blur = cv.GaussianBlur(img_gray, (5, 5), 1)
+    thres = sf.valueTrackbars()
+    img_thres = cv.Canny(img_blur, thres[0], thres[1])
+    kernel = np.ones((5, 5))
+    img_dilate = cv.dilate(img_thres, kernel, iterations=2)
+    img_erode = cv.erode(img_dilate, kernel, iterations=1)
+    
+    img_contours = img.copy()
+    img_big_contour = img.copy()
+    contours, hierarchy = cv.findContours(img_erode, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    cv.drawContours(img_contours, contours, -1, (0, 255, 0), 10)
+
+    biggest, maxArea = sf.biggestContour(contours)
+    if biggest.size != 0:
+        biggest = sf.reorder(biggest)
+        cv.drawContours(img_big_contour, biggest, -1, (0, 255, 0), 20)
+        img_big_contour = sf.drawRectangle(img_big_contour, biggest, 2)
+        pts1 = np.float32(biggest)
+        pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+        matrix = cv.getPerspectiveTransform(pts1, pts2)
+        img_warpColored = cv.warpPerspective(img, matrix, (width, height))
+        img_warpColored = img_warpColored[20:img_warpColored.shape[0] - 20, 20:img_warpColored.shape[1] - 20]
+        img_warpColored = cv.resize(img_warpColored, (width, height))
+        img_contours2 = img_warpColored.copy()
+        
+        
+
+        img_warpGray = cv.cvtColor(img_warpColored, cv.COLOR_BGR2GRAY)
+        img_warpthres = cv.adaptiveThreshold(img_warpGray, 255, 1, 1, 7, 2)
+        img_warpthres = cv.bitwise_not(img_warpthres)
+        img_warpthres = cv.medianBlur(img_warpthres, 5)
+
+        imgs = ([img, img_gray, img_thres, img_contours],
+            [img_big_contour, img_warpColored, img_warpGray, img_warpthres])
+    else:
+        imgs = ([img, img_gray, img_thres, img_contours],[img_blank, img_blank, img_blank, img_blank])
+    labels = [["oringinal", "gray", "thres", "contours"], 
+                   ["biggest", "warp colored", "warp gray", "warp thres"]]
+    stackedImages = sf.stackImages(0.5, imgs, labels)
+    cv.imshow("stacked images", stackedImages)
+    cv.waitKey(1)
---
+++ scanFunctions.py
@@ -0,0 +1,90 @@
+import cv2 as cv
+import numpy as np
+
+def nothing():
+    pass
+
+def initTrackbars():
+    cv.namedWindow("threshold")
+    cv.resizeWindow("threshold", 360, 240)
+    cv.createTrackbar("thres_min", "threshold", 0, 255, nothing)
+    cv.createTrackbar("thres_max", "threshold", 255, 255, nothing)
+
+def valueTrackbars():
+    thres_min = cv.getTrackbarPos("thres_min", "threshold")
+    thres_max = cv.getTrackbarPos("thres_max", "threshold")
+    return [thres_min, thres_max]
+
+def biggestContour(contours):
+    max_area = 0
+    biggest = np.array([])
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        if area > 5000:
+            peri = cv.arcLength(cnt, True)
+            approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest, max_area
+
+def drawRectangle(img, biggest, thickness):
+    cv.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+    cv.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+    return img
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+    
+def stackImages(scale, imgArray, labels=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv.cvtColor(imgArray[x][y], cv.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv.cvtColor(imgArray[x], cv.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        hor_con = np.concatenate(imgArray)
+        ver = hor
+    if len(labels) != 0:
+        eachImgWidth = int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        for d in range(0, rows):
+            for c in range(0, cols):
+                cv.rectangle(ver, (c*eachImgWidth, eachImgHeight*d), (c*eachImgWidth+len(labels[d][c])*13+27, 30+eachImgHeight*d), (255, 255, 255), cv.FILLED) 
+                cv.putText(ver, labels[d][c], (eachImgWidth*c+10, eachImgHeight*d+20), cv.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 255), 2)
+    return ver
